f8d3b9646f3a301e5f13552fa57405af4888c2a2
R B suraj <rbsuraj432@gmail.com>
2025-07-20 17:11:47 +0530

    gestures

--- learning_docs/src/components/Gestures.tsx
+++ learning_docs/src/components/Gestures.tsx
@@ -1,10 +1,18 @@
 'use client';
 
+import { motion } from 'motion/react';
+
 const Gestures = () => {
   return (
     <fieldset>
       <legend>Gestures</legend>
-      <div id="gesturesContainer"></div>
+      <div id="gesturesContainer">
+        <motion.div
+          whileTap={{ scale: 0.8 }}
+          whileHover={{ scale: 1.2 }}
+          className="w-24 h-24 bg-fuchsia-500 rounded-md"
+        />
+      </div>
     </fieldset>
   );
 };
