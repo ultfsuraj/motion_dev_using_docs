b2470914377097b4e61556135dbdd8b8d104d8dd
R B suraj <rbsuraj432@gmail.com>
2025-07-20 16:44:10 +0530

    enter animation separate prop for each animation property if complex

--- learning_docs/src/components/EnterAnimation.tsx
+++ learning_docs/src/components/EnterAnimation.tsx
@@ -1,10 +1,24 @@
 'use client';
 
+import { motion } from 'motion/react';
+
 const EnterAnimation = () => {
   return (
     <fieldset>
       <legend>Enter Animation</legend>
-      <div id="enterAnimationContainer"></div>
+      <div id="enterAnimationContainer" className="gap-4 flex flex-col w-36 items-center">
+        <motion.div
+          className="w-20 h-20 bg-amber-500 rounded-full"
+          initial={{ opacity: 0, scale: 0 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{
+            // better to separately specify, if duratio and delay  spring type, complexities are there
+            opacity: { delay: 1.5, duration: 0.4 },
+            scale: { delay: 1.5, type: 'spring', visualDuration: 0.4, bounce: 0.7 },
+          }}
+        />
+        <button className="py-1 px-3 bg-amber-800 rounded-md">refresh</button>
+      </div>
     </fieldset>
   );
 };
