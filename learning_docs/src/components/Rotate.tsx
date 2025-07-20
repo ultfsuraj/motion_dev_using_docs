8e79403d259ee72f22883215313bb91159ae7d9d
R B suraj <rbsuraj432@gmail.com>
2025-07-20 23:52:27 +0530

    rotate

--- learning_docs/src/components/Rotate.tsx
+++ learning_docs/src/components/Rotate.tsx
@@ -1,10 +1,20 @@
 'use client';
 
+import { motion } from 'motion/react';
+
 const Rotate = () => {
   return (
     <fieldset>
       <legend>Rotate</legend>
-      <div id="rotateContainer"></div>
+      <div id="rotateContainer">
+        <motion.div
+          className="w-20 h-20 rounded-md bg-blue-500"
+          animate={{
+            rotate: 360,
+          }}
+          transition={{ duration: 1, delay: 2 }}
+        />
+      </div>
     </fieldset>
   );
 };
