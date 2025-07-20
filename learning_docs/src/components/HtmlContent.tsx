8eeca0492a5f34b644831f51ce203f098b6299f7
R B suraj <rbsuraj432@gmail.com>
2025-07-20 22:59:08 +0530

    html content motionvalue and transform

--- learning_docs/src/components/HtmlContent.tsx
+++ learning_docs/src/components/HtmlContent.tsx
@@ -1,10 +1,23 @@
 'use client';
 
+import { animate, motion, useMotionValue, useTransform } from 'motion/react';
+import { useEffect } from 'react';
+
 const HtmlContent = () => {
+  const count = useMotionValue(0);
+  const rounded = useTransform(() => Math.round(count.get()));
+
+  useEffect(() => {
+    const controls = animate(count, 100, { duration: 5, delay: 2 });
+    return () => controls.stop();
+  }, []);
+
   return (
     <fieldset>
       <legend>HTML Content</legend>
-      <div id="htmlContainer"></div>
+      <div id="htmlContainer">
+        <motion.pre className="text-6xl text-emerald-500">{rounded}</motion.pre>
+      </div>
     </fieldset>
   );
 };
