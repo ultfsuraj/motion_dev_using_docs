dcf067e127d5813725d6bb63b038afff20856ff9
R B suraj <rbsuraj432@gmail.com>
2025-07-20 16:20:56 +0530

    drag element constraints

--- learning_docs/src/components/Drag.tsx
+++ learning_docs/src/components/Drag.tsx
@@ -1,10 +1,16 @@
 'use client';
 
+import { motion } from 'motion/react';
+import { useRef } from 'react';
+
 const Drag = () => {
+  const boundaryRef = useRef(null);
   return (
     <fieldset>
       <legend>Drag</legend>
-      <div id="dragContainer"></div>
+      <div id="dragContainer" ref={boundaryRef} className="w-1/2 h-40 border border-amber-50">
+        <motion.div className="w-20 h-20 bg-pink-700 rounded-lg" drag dragConstraints={boundaryRef} />
+      </div>
     </fieldset>
   );
 };
