e5d4b42c99f6fd7a5bb25e73d67d253ab1ead643
R B suraj <rbsuraj432@gmail.com>
2025-07-20 13:45:20 +0530

    layout for trying motiondev react free examples

--- learning_docs/src/app/page.tsx
+++ learning_docs/src/app/page.tsx
@@ -3,7 +3,82 @@
 import React from 'react';
 
 const Page = () => {
-  return <div className="content-center text-center py-4 px-8">Page</div>;
+  return (
+    <div className="py-4 px-8">
+      <fieldset>
+        <legend>CSS spring</legend>
+        <div id="springContainer"></div>
+      </fieldset>
+      <fieldset>
+        <legend>Drag</legend>
+        <div id="dragContainer"></div>
+      </fieldset>
+      <fieldset>
+        <legend>Enter Animation</legend>
+        <div id="enterContainer"></div>
+      </fieldset>
+      <fieldset>
+        <legend>Exit Animation</legend>
+        <div id="exitContainer "></div>
+      </fieldset>
+      <fieldset>
+        <legend>Gestures</legend>
+        <div id="gesturesContainer"></div>
+      </fieldset>
+      <fieldset>
+        <legend>HTML Content</legend>
+        <div id="htmlContainer"></div>
+      </fieldset>
+      <fieldset>
+        <legend>Keyframes</legend>
+        <div id="KeyframesContainer"></div>
+      </fieldset>
+      <fieldset>
+        <legend>Keyframe Wildcards</legend>
+        <div id="wildcardsContainer"></div>
+      </fieldset>
+      <fieldset>
+        <legend>Rotate</legend>
+        <div id="rotateContainer"></div>
+      </fieldset>
+      <fieldset>
+        <legend>Transition Options</legend>
+        <div id="transitionContainer"></div>
+      </fieldset>
+      <fieldset>
+        <legend>useAnimationFrame</legend>
+        <div id="useAnimationFrameContainer"></div>
+      </fieldset>
+      <fieldset>
+        <legend>Follow Pointer With Spring</legend>
+        <div id="followPointerContainer"></div>
+      </fieldset>
+      <fieldset>
+        <legend>Layout Animation</legend>
+        <div id="layoutContainer"></div>
+      </fieldset>
+      <fieldset>
+        <legend>Motion Along a Path</legend>
+        <div id="motionPathContainer"></div>
+      </fieldset>
+      <fieldset>
+        <legend>Parallax</legend>
+        <div id="parallaxContainer"></div>
+      </fieldset>
+      <fieldset>
+        <legend>Path Drawing</legend>
+        <div id="pathDrawContainer"></div>
+      </fieldset>
+      <fieldset>
+        <legend>Split Text</legend>
+        <div id="splitTextContainer"></div>
+      </fieldset>
+      <fieldset>
+        <legend>Path morphing</legend>
+        <div id="pathMorphContainer"></div>
+      </fieldset>
+    </div>
+  );
 };
 
 export default Page;
