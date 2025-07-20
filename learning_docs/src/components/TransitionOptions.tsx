eaa32feb543d8eca95827983b2e835b2bdd83ef4
R B suraj <rbsuraj432@gmail.com>
2025-07-21 01:44:14 +0530

    3d cube useAnimationFrame, transition options

--- learning_docs/src/components/TransitionOptions.tsx
+++ learning_docs/src/components/TransitionOptions.tsx
@@ -1,10 +1,24 @@
 'use client';
 
+import { motion } from 'motion/react';
+
 const TransitionOptions = () => {
   return (
     <fieldset>
       <legend>Transition Options</legend>
-      <div id="transitionOptionsContainer"></div>
+      <div id="transitionOptionsContainer">
+        <motion.div
+          className="w-24 h-24 rounded-full bg-teal-500"
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{
+            duration: 0.8,
+            delay: 1,
+            ease: [0, 0.71, 0.2, 1.01],
+            repeat: 3,
+          }}
+        />
+      </div>
     </fieldset>
   );
 };
--- learning_docs/src/components/UseAnimationFrame.tsx
+++ learning_docs/src/components/UseAnimationFrame.tsx
@@ -1,12 +1,91 @@
 'use client';
 
+import { useAnimationFrame } from 'motion/react';
+import { useRef } from 'react';
+
 const UseAnimationFrame = () => {
+  const ref = useRef<HTMLDivElement>(null);
+
+  useAnimationFrame((t) => {
+    if (!ref.current) return;
+
+    const rotate = Math.sin(t / 10000) * 200;
+    const y = (1 + Math.sin(t / 1000)) * -50;
+    ref.current.style.transform = `translateY(${y}px) rotateX(${rotate}deg) rotateY(${rotate}deg) `;
+  });
+
   return (
     <fieldset>
       <legend>Use Animation Frame</legend>
-      <div id="useAnimationFrameContainer"></div>
+      <div id="useAnimationFrameContainer">
+        <div className="container border border-white">
+          <div className="cube" ref={ref}>
+            <div className="side front"></div>
+            <div className="side left"></div>
+            <div className="side right"></div>
+            <div className="side top"></div>
+            <div className="side bottom"></div>
+            <div className="side back"></div>
+          </div>
+          <StyleSheet />
+        </div>
+      </div>
     </fieldset>
   );
 };
+
+function StyleSheet() {
+  return (
+    <style>{`
+            
+        .container {
+            perspective: 800px;
+            width: 200px;
+            height: 200px;
+        }
+
+        .cube {
+            width: 100px;
+            height: 100px;
+            position: relative;
+            transform-style: preserve-3d;
+        }
+
+        .side {
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            background-color: red;
+            opacity: 0.6;
+        }
+
+        .front {
+            transform: rotateY(0deg) translateZ(50px);
+            background-color:  red;
+        }
+        .right {
+            transform: rotateY(90deg) translateZ(50px);
+            background-color: yellow;
+        }
+        .back {
+            transform: rotateY(180deg) translateZ(50px);
+            background-color:  green;
+        }
+        .left {
+            transform: rotateY(-90deg) translateZ(50px);
+            background-color: blue;
+        }
+        .top {
+            transform: rotateX(90deg) translateZ(50px);
+            background-color: white;
+        }
+        .bottom {
+            transform: rotateX(-90deg) translateZ(50px);
+            background-color: cyan;
+        }
+
+    `}</style>
+  );
+}
 
 export default UseAnimationFrame;
