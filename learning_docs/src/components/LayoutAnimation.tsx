c30f7fc836ac33d6db87943965db0e289feedf81
R B suraj <rbsuraj432@gmail.com>
2025-07-21 11:21:13 +0530

    layout animation

--- learning_docs/src/components/LayoutAnimation.tsx
+++ learning_docs/src/components/LayoutAnimation.tsx
@@ -1,10 +1,65 @@
 'use client';
 
+import { motion } from 'motion/react';
+import { useState } from 'react';
+
 const LayoutAnimation = () => {
+  const [isOn, setIsOn] = useState(false);
+  const toggleswitch = () => setIsOn(!isOn);
+
   return (
     <fieldset>
       <legend>Layout Animation</legend>
-      <div id="layoutAnimationContainer"></div>
+      <div id="layoutAnimationContainer">
+        <motion.button
+          className=" h-14 cursor-pointer flex items-center gap-2 p-2 rounded-md bg-zinc-50 "
+          style={{ justifyContent: isOn ? 'start' : 'end', flexDirection: isOn ? 'row' : 'column' }}
+          onClick={toggleswitch}
+          layout
+          transition={{
+            type: 'spring',
+            visualDuration: 0.2,
+            bounce: 0.2,
+          }}
+        >
+          <motion.div
+            className="h-10 w-10 rounded-md bg-zinc-800"
+            layout
+            transition={{
+              type: 'spring',
+              visualDuration: 0.2,
+              bounce: 0.2,
+            }}
+          />
+          <motion.div
+            className="h-10 w-10 rounded-md bg-zinc-800"
+            layout
+            transition={{
+              type: 'spring',
+              visualDuration: 0.2,
+              bounce: 0.2,
+            }}
+          />
+          <motion.div
+            className="h-10 w-10 rounded-md bg-zinc-800"
+            layout
+            transition={{
+              type: 'spring',
+              visualDuration: 0.2,
+              bounce: 0.2,
+            }}
+          />
+          <motion.div
+            className="h-10 w-10 rounded-md bg-zinc-800"
+            layout
+            transition={{
+              type: 'spring',
+              visualDuration: 0.2,
+              bounce: 0.2,
+            }}
+          />
+        </motion.button>
+      </div>
     </fieldset>
   );
 };
